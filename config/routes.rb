f819ce7432c85ded76cb81cfc033f9b4245a908c
ipeinado <i.peinado.martinez@gmail.com>
2015-05-12 15:03:45 +0200

    added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users, :path => '', :path_names => {:sign_in => 'entrar', :sign_out => 'salir'}
+  
+  resources :users, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
