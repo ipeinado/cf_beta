6be189d6230ab8835643dd3ffe1e32763cd700bc
Ignacio Peinado <i.peinado.martinez@gmail.com>
2015-05-14 01:11:53 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,15 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  # Users routes
-  get 'registrarse' => 'users#new'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  # Users routes
+  resources :users
+
 
   # Example resource route with options:
   #   resources :products do
