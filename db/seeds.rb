72aae396481649a0c4b4351c2b5fd47d47ab93b3
Ignacio Peinado <i.peinado.martinez@gmail.com>
2015-10-07 00:03:54 +0200

    added remote urls for project seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,8 @@ Project.create!(title: "Homelessfonts",
 					description: "Homelessfonts is an Arrels Foundation initiative which consists of creating a collection of typefaces based on the handwriting of homeless",
 					website: "http://www.homelessfonts.org/",
 					twitter: "homelessfonts",
-					facebook: "https://www.facebook.com/homelessfonts")
+					facebook: "https://www.facebook.com/homelessfonts",
+					remote_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/logo/1/Homelessfonts_LOGO.png")
 
 Project.create!(title: "joyners",
 					address: "Calle Calàbria 169",
@@ -36,7 +37,9 @@ Project.create!(title: "joyners",
 					description: "¡Comparte piso con séniors afines, divide gastos y disfruta de tu etapa adulta mientras accedes a múltiples servicios!",
 					website: "http://www.joyners.es/",
 					twitter: "somosjoyners",
-					facebook: "https://www.facebook.com/pages/Joyners/1537823113160952")
+					facebook: "https://www.facebook.com/pages/Joyners/1537823113160952",
+					remote_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/logo/2/Joiners_LOGO.jpeg",
+					remote_featured_picture_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/featured_picture/2/Joyners_IMG_DESTACADA.jpg")
 
 Project.create!(title: "Accessible.info",
 					address: "Calle Madres de la Plaza de Mayo, 44, 31013",
@@ -47,7 +50,8 @@ Project.create!(title: "Accessible.info",
 					description: "Buscador de lugares accesibles con información del lugar, fotos... Sitios accesibles para mascotas. Auditorias para obtener grado de accesibilidad A, B , C.",
 					website: "http://www.accessible.info/",
 					twitter: "accessible_info",
-					facebook: "https://www.facebook.com/accesible.paratodos")
+					facebook: "https://www.facebook.com/accesible.paratodos",
+					remote_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/logo/3/AccesibleInfo_LOGO.png")
 
 Project.create!(title: "Inrex Home Care, LLC",
 					address: "415 W. Schrock Rd.",
@@ -57,7 +61,8 @@ Project.create!(title: "Inrex Home Care, LLC",
 					description: "INREX Home Care, LLC is committed to providing the highest quality of services and support to our community members with physical and developmental disabilities by helping them live their lives to their fullest potential",
 					website: "http://www.inrexhomecare.com",
 					twitter: "InrexHomeCare",
-					facebook: "https://www.facebook.com/pages/Inrex-Home-Care-LLC/1537934039754844")
+					facebook: "https://www.facebook.com/pages/Inrex-Home-Care-LLC/1537934039754844",
+					remote_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/logo/4/InrexHomeCare_LOGO.jpg")
 
 Project.create!(title: "VecineoApp",
 					city: "Murcia",
@@ -66,7 +71,9 @@ Project.create!(title: "VecineoApp",
 					description: "Ofrece tus servicios a tus vecinos y encuentra ofertas con nuestra App. Vecineo, tu vida en una milla",
 					website: "http://www.vecineo.com/",
 					twitter: "VecineoApp",
-					facebook: "https://www.facebook.com/Vecineo")
+					facebook: "https://www.facebook.com/Vecineo",
+					remote_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/logo/5/VecineoAPP_LOGO.png",
+					remote_featured_picture_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/featured_picture/5/VecineoApp_IMG_DESTACADA.png")
 
 Project.create!(title: "BCN ACCESSTOUR",
 					city: "Barcelona",
@@ -74,7 +81,8 @@ Project.create!(title: "BCN ACCESSTOUR",
 					country: "España",
 					description: "Inbound Tour Operator specialized in #accessibletourism in #Spain. #accessibleholidays for special needs families, senior and PRM.",
 					website: "http://www.bcnaccestour.com/",
-					twitter: "bcnaccestour")
+					twitter: "bcnaccestour",
+					remote_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/logo/6/BCNAccessTour_LOGO.jpeg")
 
 Project.create!(title: "Ciudad Fácil",
 					city: "Valdivia",
@@ -83,7 +91,8 @@ Project.create!(title: "Ciudad Fácil",
 					description: "Aplicación móvil inclusiva, para que todos disfrutemos de la ciudad. ¿Te unes en esta aventura :D? Beta disponible en playstore!",
 					website: "http://ciudadfacil.cl/",
 					twitter: "ciudadFacil",
-					facebook: "https://www.facebook.com/ciudadFacil.cl")
+					facebook: "https://www.facebook.com/ciudadFacil.cl",
+					remote_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/logo/7/CiudadFacil_LOGO.jpg")
 
 Project.create!(title: "encicle",
 					address: "C/ Zapateros, 8",
@@ -93,7 +102,8 @@ Project.create!(title: "encicle",
 					description: "Somos una empresa de bicimensajeros valenciana comprometida con las necesidades de nuestra ciudad de una forma sostenible, eficaz y respetuosa.",
 					website: "http://www.enciclebicimensajeros.com/",
 					twitter: "encicle",
-					facebook: "https://www.facebook.com/enciclebicimensajeros")
+					facebook: "https://www.facebook.com/enciclebicimensajeros",
+					remote_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/logo/8/Encicle_LOGO.jpg")
 
 Project.create!(title: "Mi Huerto Web",
 					city: "Huellelhue",
@@ -102,7 +112,8 @@ Project.create!(title: "Mi Huerto Web",
 					description: "Agricultura sustentable, comercio justo e inclusivo. La nueva manera de obtener tus productos del campo, colaborando directamente con la agricultura familiar campesina.",
 					website: "http://mihuertoweb.cl/",
 					twitter: "MiHuertoWeb",
-					facebook: "https://www.facebook.com/MiHuertoWeb")
+					facebook: "https://www.facebook.com/MiHuertoWeb",
+					remote_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/logo/9/MiHuertoWeb_LOGO.jpg")
 
 Project.create!(title: "Huerta Montecarmelo",
 					address: "Monasterio de las Huelgas, 15",
@@ -112,7 +123,9 @@ Project.create!(title: "Huerta Montecarmelo",
 					description: "huertos urbanos en pleno Madrid, con trabajadores con discapacidad intelectual",
 					website: "http://www.lahuertademontecarmelo.com/",
 					twitter: "HuertaMtcarmelo",
-					facebook: "https://www.facebook.com/LaHuertaDeMontecarmelo")
+					facebook: "https://www.facebook.com/LaHuertaDeMontecarmelo",
+					remote_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/logo/10/HuertaMontecarmelo_LOGO.jpg",
+					remote_featured_picture_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/project/featured_picture/10/HuertaMonte_IMG_DESTACADA.jpg")
 
 Project.create!(title: "@Trazeo",
 					city: "Córdoba",
@@ -131,7 +144,9 @@ balsamiq = Promo.create!(name: "Balsamiq",
 	about_the_promo: "3 months free extended trial of myBalsamiq with full functionality. The offer applies on new myBalsamiq accounts only and cannot be used towards existing accounts.",
   twitter: "http://twitter.com/balsamiq",
   facebook: "http://facebook.com/balsamiq",
-  website: "http://balsamiq.com"
+  website: "http://balsamiq.com",
+	remote_promo_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/promo/promo_logo/1/Balsamiq_LOGO.png",
+	remote_promo_featured_picture_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/promo/promo_featured_picture/1/Balsamic_ImagenDestacada_Cuerpo.jpg.png"
   )
 
 w = Category.create!(name: "Web")
@@ -146,7 +161,8 @@ trello = Promo.create!(name: "Trello Gold",
 	about_the_promo: "Get a $15 discount on Trello Gold",
   twitter: "http://twitter.com/trello",
   facebook: "https://www.facebook.com/trelloapp",
-  website: "https://trello.com/"
+  website: "https://trello.com/",
+	remote_promo_logo_url: "https://s3.eu-central-1.amazonaws.com/cfbeta/uploads/promo/promo_logo/2/Trello_LOGO.png"
 )
 
 trello.categories.create(name: "Project management")
