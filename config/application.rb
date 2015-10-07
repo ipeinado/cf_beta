d6b022adb129a0af7831492b181428e3c735fde7
Ignacio Peinado <i.peinado.martinez@gmail.com>
2015-10-07 17:05:38 +0200

    added config.serve_static_assets = true to application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module CfV01
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.serve_static_assets = true
     config.serve_static_files = true
     config.assets.initialize_on_precompile = false
 
