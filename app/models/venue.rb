7d12af3a87918c4c05257eeedb105d210c096296
Ignacio Peinado <i.peinado.martinez@gmail.com>
2015-11-22 23:50:44 +0100

    added location to venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,8 +4,16 @@ class Venue < ActiveRecord::Base
 
   mount_uploader :venue_picture, VenuePictureUploader
 
+  geocoded_by :venue_location
+  after_validation :geocode
+
   validates :name, presence: true
   validates :city, presence: true
   validates :country, presence: true
+
+  protected
+    def venue_location
+      [address, city, province, country].compact.join(', ')
+    end
 
 end
